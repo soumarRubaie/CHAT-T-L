9776996dec7b419ea878c1ca0ccfc27b14382ba9
soumar alrubaie <soumarruu@gmail.com>
2018-03-31 09:05:45 +0800

    ajouter authentification

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,28 @@
+import java.io.IOException;
+import com.google.gson.JsonObject;
+
+import Serveur.AuthentificationUser;
+import Serveur.Requests;
+import Serveur.Requests.RequestType;
+import jdk.nashorn.internal.ir.RuntimeNode.Request;
+
+
 
 public class Main {
 
+	private static Main instance;
+	public static boolean arret_Serveur = false;
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("test First commit projet Chat");
+		JsonObject obj = new JsonObject();	
+		obj.addProperty("username", "soumar");
+		obj.addProperty("password", "password");
+		
+		Requests requests= new AuthentificationUser(obj); 
+		
+		System.out.println(requests.getType()); 
+
 	}
 
 }
