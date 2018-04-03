496e37597653afcd7de7d2fd4d8063bbb532c48b
Aziz zouaoui <zoua1703@usherbrooke.ca>
2018-04-03 13:33:38 -0400

    Modification "creation d'un compte"

--- src/Client/Home.java
+++ src/Client/Home.java
@@ -1,4 +1,5 @@
 package Client;
+
 import java.awt.Font;
 
 import javax.swing.JButton;
@@ -9,11 +10,18 @@ import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import java.awt.Color;
 import javax.swing.JTable;
+import javax.swing.JList;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
+import javax.swing.border.TitledBorder;
 
 public class Home {
 	private static JPasswordField passwordField;
 	private static JPanel panel_2;
-	private static JTable table;
+	static String label[] = { "Zero", "One", "Two", "Three", "Four", "Five", "Six",
+		      "Seven", "Eight", "Nine", "Ten", "Eleven" };
+	static String labelSalleDiscussion[] = { "Salle Zero", "Salle One", "Salle Two", "Salle Three", "Salle Four", "Salle Five", "Salle Six",
+		      "Salle Seven", "Salle Eight", "Salle Nine", "Salle Ten", "Salle Eleven" };
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
@@ -38,7 +46,7 @@ public class Home {
 		
 //		private static final Action action = new SwingAction();
 
-			JFrame frame = new JFrame("formulaire d'inscription");
+			JFrame frame = new JFrame("Home");
 			frame.setSize(1040, 761);
 			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -59,11 +67,19 @@ public class Home {
 			panel_1.setBounds(12, 13, 998, 690);
 			panel_2.add(panel_1);
 			panel_1.setLayout(null);
-
-			JLabel userLabel = new JLabel("Liste d'utilisateurs:");
-			userLabel.setBounds(12, 15, 151, 25);
-			panel_1.add(userLabel);
-			userLabel.setFont(new Font("Tahoma", Font.PLAIN, 17));
+			
+			JPanel panel_4 = new JPanel();
+			panel_4.setBorder(new TitledBorder(null, "Liste d'utilisateurs:", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+			panel_4.setBounds(12, 21, 249, 196);
+			panel_1.add(panel_4);
+			panel_4.setLayout(null);
+			
+			JScrollPane scrollPane = new JScrollPane();
+			scrollPane.setBounds(12, 26, 225, 161);
+			panel_4.add(scrollPane);
+			
+			JList list = new JList(label);
+			scrollPane.setViewportView(list);
 
 			JTextField userText = new JTextField(20);
 			userText.setBounds(826, 196, 160, 25);
@@ -100,9 +116,22 @@ public class Home {
 			btnDeconnexion.setBounds(852, 2, 134, 51);
 			panel_1.add(btnDeconnexion);
 			
-			table = new JTable();
-			table.setBounds(22, 64, 142, 168);
-			panel_1.add(table);
+			JPanel panel_3 = new JPanel();
+			panel_3.setBorder(new TitledBorder(null, "Salle de discussion:", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+			panel_3.setBounds(12, 230, 249, 250);
+			panel_1.add(panel_3);
+			panel_3.setLayout(null);
+			
+			JScrollPane scrollPane_1 = new JScrollPane();
+			scrollPane_1.setBounds(6, 34, 231, 170);
+			panel_3.add(scrollPane_1);
+			
+			JList list_1 = new JList(labelSalleDiscussion);
+			scrollPane_1.setViewportView(list_1);
+			
+			JButton btnNewButton = new JButton("Rejoindre discussion");
+			btnNewButton.setBounds(27, 212, 169, 25);
+			panel_3.add(btnNewButton);
 			
 			
 			
--- src/Client/Inscription.java
+++ src/Client/Inscription.java
@@ -14,11 +14,14 @@ import javax.swing.Action;
 public class Inscription {
 	private static JPasswordField passwordField;
 	private static JPanel panel_2;
+	private static JTextField textField;
+	private static JTextField textField_1;
+	private static JTextField textField_2;
 //	private static final Action action = new SwingAction();
 
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("formulaire d'inscription");
-		frame.setSize(546, 449);
+		frame.setSize(546, 559);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		panel_2 = new JPanel();
@@ -35,43 +38,70 @@ public class Inscription {
 		panel.setLayout(null);
 		
 		JPanel panel_1 = new JPanel();
-		panel_1.setBounds(23, 43, 390, 287);
+		panel_1.setBounds(23, 13, 480, 488);
 		panel_2.add(panel_1);
 		panel_1.setLayout(null);
 
-		JLabel userLabel = new JLabel("Utilisateur");
-		userLabel.setBounds(0, 0, 80, 25);
+		JLabel userLabel = new JLabel("Nom d'utilisateur*");
+		userLabel.setBounds(12, 178, 149, 25);
 		panel_1.add(userLabel);
 		userLabel.setFont(new Font("Tahoma", Font.PLAIN, 17));
 
 		JTextField userText = new JTextField(20);
-		userText.setBounds(230, 2, 160, 25);
+		userText.setBounds(242, 180, 160, 25);
 		panel_1.add(userText);
 
-		JLabel passwordLabel = new JLabel("Mot de passe");
-		passwordLabel.setBounds(0, 64, 104, 25);
+		JLabel passwordLabel = new JLabel("Mot de passe*");
+		passwordLabel.setBounds(12, 206, 114, 25);
 		panel_1.add(passwordLabel);
 		passwordLabel.setFont(new Font("Tahoma", Font.PLAIN, 17));
 
 		JPasswordField passwordText = new JPasswordField(20);
-		passwordText.setBounds(230, 66, 160, 25);
+		passwordText.setBounds(242, 208, 160, 25);
 		panel_1.add(passwordText);
 		
 		JButton registerButton = new JButton("Inscription");
 //		registerButton.setAction(action);
-		registerButton.setBounds(155, 236, 134, 51);
+		registerButton.setBounds(167, 378, 134, 51);
 		panel_1.add(registerButton);
 		registerButton.setFont(new Font("Tahoma", Font.PLAIN, 17));
 		
 		passwordField = new JPasswordField(20);
-		passwordField.setBounds(230, 143, 160, 25);
+		passwordField.setBounds(242, 285, 160, 25);
 		panel_1.add(passwordField);
 		System.out.printf("valeur du input", passwordField);
 		
-		JLabel lblConfirmerMotDe = new JLabel("Confirmer mot de passe");
-		lblConfirmerMotDe.setBounds(0, 128, 204, 51);
+		JLabel lblConfirmerMotDe = new JLabel("Confirmer mot de passe*");
+		lblConfirmerMotDe.setBounds(12, 270, 204, 51);
 		panel_1.add(lblConfirmerMotDe);
 		lblConfirmerMotDe.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		
+		JLabel lblNom = new JLabel("Nom");
+		lblNom.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		lblNom.setBounds(12, 13, 149, 25);
+		panel_1.add(lblNom);
+		
+		textField = new JTextField(20);
+		textField.setBounds(242, 15, 160, 25);
+		panel_1.add(textField);
+		
+		JLabel lblPays = new JLabel("Pays");
+		lblPays.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		lblPays.setBounds(12, 51, 149, 25);
+		panel_1.add(lblPays);
+		
+		textField_1 = new JTextField(20);
+		textField_1.setBounds(242, 53, 160, 25);
+		panel_1.add(textField_1);
+		
+		JLabel lblDescription = new JLabel("Description");
+		lblDescription.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		lblDescription.setBounds(12, 89, 149, 25);
+		panel_1.add(lblDescription);
+		
+		textField_2 = new JTextField(20);
+		textField_2.setBounds(242, 91, 160, 76);
+		panel_1.add(textField_2);
 		
 		
 		
