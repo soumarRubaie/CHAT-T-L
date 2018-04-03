c3f9aa3f3353e9bf54853ab818ddc182c15222ad
Aziz zouaoui <zoua1703@usherbrooke.ca>
2018-04-03 16:43:27 -0400

    Version du home pas encore terminée

--- src/Client/Home.java
+++ src/Client/Home.java
@@ -1,22 +1,20 @@
-package Client;
 
 import java.awt.Font;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JPasswordField;
-import javax.swing.JTextField;
 import java.awt.Color;
-import javax.swing.JTable;
 import javax.swing.JList;
-import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
 import javax.swing.border.TitledBorder;
+import javax.swing.AbstractAction;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Home {
-	private static JPasswordField passwordField;
+import javax.swing.Action;
+
+public class Home implements ActionListener  {
 	private static JPanel panel_2;
 	static String label[] = { "Zero", "One", "Two", "Three", "Four", "Five", "Six",
 		      "Seven", "Eight", "Nine", "Ten", "Eleven" };
@@ -59,66 +57,45 @@ public class Home {
 			
 		}
 
-		private static void placeComponents(JPanel panel) {
+		public static  void placeComponents(JPanel panel) {
 
 			panel.setLayout(null);
 			
 			JPanel panel_1 = new JPanel();
-			panel_1.setBounds(12, 13, 998, 690);
+			panel_1.setBounds(12, -14, 998, 690);
 			panel_2.add(panel_1);
 			panel_1.setLayout(null);
 			
 			JPanel panel_4 = new JPanel();
 			panel_4.setBorder(new TitledBorder(null, "Liste d'utilisateurs:", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-			panel_4.setBounds(12, 21, 249, 196);
+			panel_4.setBounds(0, 2, 249, 260);
 			panel_1.add(panel_4);
 			panel_4.setLayout(null);
 			
 			JScrollPane scrollPane = new JScrollPane();
-			scrollPane.setBounds(12, 26, 225, 161);
+			scrollPane.setBounds(12, 26, 225, 185);
 			panel_4.add(scrollPane);
 			
 			JList list = new JList(label);
 			scrollPane.setViewportView(list);
-
-			JTextField userText = new JTextField(20);
-			userText.setBounds(826, 196, 160, 25);
-			panel_1.add(userText);
-
-			JLabel passwordLabel = new JLabel("Mot de passe");
-			passwordLabel.setBounds(596, 258, 104, 25);
-			panel_1.add(passwordLabel);
-			passwordLabel.setFont(new Font("Tahoma", Font.PLAIN, 17));
-
-			JPasswordField passwordText = new JPasswordField(20);
-			passwordText.setBounds(826, 260, 160, 25);
-			panel_1.add(passwordText);
 			
-			JButton registerButton = new JButton("Inscription");
-//			registerButton.setAction(action);
-			registerButton.setBounds(651, 399, 134, 51);
-			panel_1.add(registerButton);
-			registerButton.setFont(new Font("Tahoma", Font.PLAIN, 17));
+			JButton btnConsulterProfil = new JButton("Consulter profil");
+			btnConsulterProfil.setBounds(36, 224, 169, 25);
+			panel_4.add(btnConsulterProfil);
+//			btnConsulterProfil.addActionListener();
+
 			
-			passwordField = new JPasswordField(20);
-			passwordField.setBounds(826, 337, 160, 25);
-			panel_1.add(passwordField);
-			System.out.printf("valeur du input", passwordField);
-			
-			JLabel lblConfirmerMotDe = new JLabel("Confirmer mot de passe");
-			lblConfirmerMotDe.setBounds(596, 322, 204, 51);
-			panel_1.add(lblConfirmerMotDe);
-			lblConfirmerMotDe.setFont(new Font("Tahoma", Font.PLAIN, 17));
 			
 			JButton btnDeconnexion = new JButton("Deconnexion");
 			btnDeconnexion.setForeground(Color.RED);
 			btnDeconnexion.setFont(new Font("Tahoma", Font.PLAIN, 17));
-			btnDeconnexion.setBounds(852, 2, 134, 51);
+			btnDeconnexion.setBounds(0, 639, 134, 51);
 			panel_1.add(btnDeconnexion);
+
 			
 			JPanel panel_3 = new JPanel();
 			panel_3.setBorder(new TitledBorder(null, "Salle de discussion:", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-			panel_3.setBounds(12, 230, 249, 250);
+			panel_3.setBounds(0, 264, 249, 250);
 			panel_1.add(panel_3);
 			panel_3.setLayout(null);
 			
@@ -133,7 +110,27 @@ public class Home {
 			btnNewButton.setBounds(27, 212, 169, 25);
 			panel_3.add(btnNewButton);
 			
+			JButton btnModifierMonProfil = new JButton("Modifier mon profil");
+			btnModifierMonProfil.setForeground(Color.RED);
+			btnModifierMonProfil.setFont(new Font("Tahoma", Font.PLAIN, 12));
+			btnModifierMonProfil.setBounds(0, 587, 134, 51);
+			panel_1.add(btnModifierMonProfil);
+			
 			
 			
 		}
+	private class SwingAction extends AbstractAction {
+		public SwingAction() {
+			putValue(NAME, "SwingAction");
+			putValue(SHORT_DESCRIPTION, "Some short description");
+		}
+		public void actionPerformed(ActionEvent e) {
+		}
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("TEST");
+		
+	}
 }
