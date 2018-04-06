1121c1ecf6435c4ee8fc86b24eb2f56e2596ba23
Soumar Alrubaie <rabs1703@usherbrooke.ca>
2018-04-06 15:38:43 -0400

    petites modifications sur l'interface

---
+++ src/Client/AjouterSalle.java
@@ -0,0 +1,21 @@
+package Client;
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.SpringLayout;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JPasswordField;
+
+public class AjouterSalle extends JFrame {
+		
+
+}
+
+	
+
